a5111a75c6492af6d3743b72a3203ec0fa470bfd
kikiramadhan075 <kikiramadhan028@gmail.com>
2024-11-02 12:19:16 +0700

    Add CRUD users, product, get API Login

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; 2024 <div class="bullet"></div> Design By <a href="https://dekref.blue/">Developer Kreatif Nusantara
+        </a>
+    </div>
+    <div class="footer-right">
+        2.3.0
+    </div>
+</footer>
