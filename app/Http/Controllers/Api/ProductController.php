3a058b2e4225b16e04dbc8813167ea94de2da1d6
kikiramadhan075 <kikiramadhan028@gmail.com>
2024-11-02 14:53:26 +0700

    get API Product

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -12,7 +12,13 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        //all products
+        $products = \App\Models\Product::orderBy('id', 'desc')->get();
+        return response()->json([
+            'success' => true,
+            'message' => 'List Data Product',
+            'data' => $products
+        ], 200);
     }
 
     /**
@@ -20,7 +26,37 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // $request->validate([
+        //     'name' => 'required|min:3',
+        //     'price' => 'required|integer',
+        //     'stock' => 'required|integer',
+        //     'category' => 'required|in:food,drink,snack',
+        //     'image' => 'required|image|mimes:png,jpg,jpeg'
+        // ]);
+
+        // $filename = time() . '.' . $request->image->extension();
+        // $request->image->storeAs('public/products', $filename);
+        // $product = \App\Models\Product::create([
+        //     'name' => $request->name,
+        //     'price' => (int) $request->price,
+        //     'stock' => (int) $request->stock,
+        //     'category' => $request->category,
+        //     'image' => $filename,
+        //     'is_favorite' => $request->is_favorite
+        // ]);
+
+        // if ($product) {
+        //     return response()->json([
+        //         'success' => true,
+        //         'message' => 'Product Created',
+        //         'data' => $product
+        //     ], 201);
+        // } else {
+        //     return response()->json([
+        //         'success' => false,
+        //         'message' => 'Product Failed to Save',
+        //     ], 409);
+        // }
     }
 
     /**
